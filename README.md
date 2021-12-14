e6ada575469adf97f887769a235f0f8e716e3eb9
Isaac Oliveira <caa6six@gmail.com>
2021-12-14 06:33:03 -0300

    commit opovo-teste-front

---
+++ README.md
@@ -0,0 +1,36 @@
+<p align=center>
+ <img src="https://github.com/Isaac0liveira/opovo-teste-frontend/blob/master/assets/o-povo-online.png">
+ </p>
+<br>
+<h1> O POVO Online - Teste Front-End </h1>
+<h4> Este é um projeto feito com o intuito de demonstrar minhas habilidades, desenvolvendo uma landpage responsiva para o jornal O POVO. <br>
+     O objetivo deste desafio é avaliar as minhas competências técnicas na construção de uma webpage em HTML. </h4>
+<br> 
+<h1> Tecnologias Utilizadas </h1>
+<h4> As seguintes ferramentas foram usadas na construção do projeto: </h4>
+<ul>
+     <li> HTML 5 </li>
+     <li> CSS 3 </li>
+     <li> JavaScript </li>
+     <li> Bootstrap 4 </li>
+     <li> Wai ARIA </li>
+</ul>
+<br>
+<h1> Responsividade </h1>
+<h4> Toda a estilização da webpage foi adaptada para diversos modelos de telas, tornando a utilização e navegação perfeita em qualquer dispositivo. </h4>
+<p align=center>
+ <img src="https://github.com/Isaac0liveira/opovo-teste-frontend/blob/master/assets/responsive-1.png" width="460" height="300">
+ </p>
+ <p align=center>
+ <img src="https://github.com/Isaac0liveira/opovo-teste-frontend/blob/master/assets/responsive-2.png" width="460" height="300">
+ </p>
+ <p align=center>
+ <img src="https://github.com/Isaac0liveira/opovo-teste-frontend/blob/master/assets/responsive-3.png" width="333" height="559">
+ </p>
+ <p align=center>
+ <img src="https://github.com/Isaac0liveira/opovo-teste-frontend/blob/master/assets/responsive-4.png" width="340" height="759">
+ </p>
+ <br>
+ <h1> Acessibilidade </h1>
+ <h4> A webpage foi adaptada com a utilização de tags ARIA para prover maior acessibilidade a pessoas com deficiência ao projeto. <br> <br>
+      Textos e botões mais complexos foram reformulados para passarem maiores informações ao usuário que utiliza leitores de tela, assim como foi utilizado Bootstrap no projeto,  cuja biblioteca já possui adaptação de acessibilidade para informar seções recolhidas ou abertas como o menu de navegação do projeto em questão. </h4>
---
+++ js/scripts.js
@@ -0,0 +1,81 @@
+(function () {
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('validacao');
+        var cep = document.getElementById("formCep");
+        var numero = document.getElementById("formNumero");
+        var curriculo = document.getElementById("formCurriculo");
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (cep.value.length != 9) {
+                    cep.classList.add("is-invalid");
+                    event.preventDefault();
+                    event.stopPropagation();
+                } else {
+                    cep.classList.contains("is-invalid") ? cep.classList.remove("is-invalid") : null;
+                }
+                if (numero.value.length != 14) {
+                    numero.classList.add("is-invalid");
+                    event.preventDefault();
+                    event.stopPropagation();
+                } else {
+                    numero.classList.contains("is-invalid") ? numero.classList.remove("is-invalid") : null;
+                }
+                if (curriculo.value == "") {
+                    document.getElementById("curriculo-text").innerHTML = "<br>Por favor, anexe o seu currículo!";
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('validado');
+            }, false);
+        });
+    }, false);
+})
+();
+
+window.onload = () => {
+    const inputCep = document.getElementById('formCep');
+    const inputNumero = document.getElementById('formNumero');
+    inputCep.onpaste = e => e.preventDefault();
+    inputNumero.onpaste = e => e.preventDefault();
+}
+
+function mascara_cep() {
+    var key = event.keyCode || event.charCode;
+    if (key < 95 || key > 106) {
+        key != 8 ? event.preventDefault() : null;
+    }
+    var cep = document.getElementById("formCep");
+    console.log(cep.value)
+    if (key != 8 && cep.value.length == 5) {
+        cep.value += "-";
+    }
+}
+
+function mascara_numero() {
+    var key = event.keyCode || event.charCode;
+    if (key < 95 || key > 106) {
+        key != 8 ? event.preventDefault() : null;
+    }
+    var numero = document.getElementById("formNumero");
+    if (key != 8) {
+        numero.value.length == 0 ? numero.value += "(" : null;
+        numero.value.length == 3 ? numero.value += ")9" : null;
+        numero.value.length == 9 ? numero.value += "-" : null;
+    }
+}
+
+function curriculo_anexo(){
+    var curriculo = document.getElementById("formCurriculo");
+    console.log(curriculo.value.length);
+    if(curriculo.value != null && curriculo.value.length != 0){
+        document.getElementById("curriculo-text").innerHTML = `<br>Anexo: ${curriculo.value}`;
+    }else{
+        document.getElementById("curriculo-text").innerHTML = `Todos os campos são obrigatórios<br>Tamanho máximo para upload - 10mb<br>Tipo de arquivo: .pdf ou .doc`;
+    }
+}
