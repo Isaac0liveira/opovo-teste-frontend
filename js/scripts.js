609ca718feb96f60afe18ceae30993c92dce88d1
Isaac Oliveira <caa6six@gmail.com>
2021-12-14 19:35:06 -0300

    commit opovo-teste-front

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 (function () {
     window.addEventListener('load', function () {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+
         var forms = document.getElementsByClassName('validacao');
         var cep = document.getElementById("formCep");
         var numero = document.getElementById("formNumero");
         var curriculo = document.getElementById("formCurriculo");
-        // Loop over them and prevent submission
+
         var validation = Array.prototype.filter.call(forms, function (form) {
             form.addEventListener('submit', function (event) {
                 if (cep.value.length != 9) {
@@ -51,7 +51,6 @@ function mascara_cep() {
         key != 8 ? event.preventDefault() : null;
     }
     var cep = document.getElementById("formCep");
-    console.log(cep.value)
     if (key != 8 && cep.value.length == 5) {
         cep.value += "-";
     }
@@ -72,7 +71,6 @@ function mascara_numero() {
 
 function curriculo_anexo(){
     var curriculo = document.getElementById("formCurriculo");
-    console.log(curriculo.value.length);
     if(curriculo.value != null && curriculo.value.length != 0){
         document.getElementById("curriculo-text").innerHTML = `<br>Anexo: ${curriculo.value}`;
     }else{
